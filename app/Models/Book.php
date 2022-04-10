80a422577701c2f6f2810cb77e4acb6304c5e96a
Tahsin Gökalp Şaan <tahsinsaan@gmaill.com>
2022-04-10 13:57:45 +0300

    ide helper yüklenmesi

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $cover
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\BookPage[] $pages
+ * @property-read int|null $pages_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCover($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     protected $fillable = [
--- app/Models/BookPage.php
+++ app/Models/BookPage.php
@@ -4,6 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\BookPage
+ *
+ * @property int $id
+ * @property int $book_id
+ * @property int $page_order
+ * @property string $image
+ * @property string|null $sound
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage wherePageOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereSound($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookPage whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BookPage extends Model
 {
     protected $fillable = [
