3206c2b43e138129af0eec89897a65779108c93f
TahsinGokalp <TahsinGokalp@users.noreply.github.com>
2022-04-19 19:59:42 +0000

    Apply fixes from StyleCI

--- app/Models/BookPage.php
+++ app/Models/BookPage.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property string|null $sound
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|BookPage newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BookPage newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BookPage query()
